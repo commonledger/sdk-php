7d343e72c53a7af6a689ceaf01cd6af4a491e6ee
Andrew Edwards <andrew@commonledger.com>
2014-08-18 09:10:05 +1200

    Added $addon_id
    The addon Id is now to be passed when a new instance of the addon class is created to reflect the end point that is being created.

--- lib/Api/Ledger/Addon.php
+++ lib/Api/Ledger/Addon.php
@@ -12,20 +12,23 @@ class Addon extends AbstractEndpoint
 
     private $ledger_id;
     private $endpoint = 'addon';
+    private $addon_id;
 
     /**
      * Create a new Addon endpoint relative to a Ledger
      *
      * @param string $prefix
      * @param string $ledger_id
+     * @param string $addon_id
      * @param HttpClient $client
      */
-    public function __construct($prefix, $ledger_id, HttpClient $client)
+    public function __construct($prefix, $ledger_id, $addon_id, HttpClient $client)
     {
         parent::__construct($client);
 
         $this->ledger_id = $ledger_id;
         $this->endpoint = sprintf('%s/%s/%s', $prefix, $ledger_id, $this->endpoint);
+        $this->addon_id = $addon_id;
     }
 
 
@@ -53,16 +56,15 @@ class Addon extends AbstractEndpoint
      *
      * Get an Addon from the current Ledger by the Addon id
      *
-     * @param string $addon_id The UUID of the Addon to fetch
      * @param array $options Optional arguments to pass to pass to the request
      *
      * @return \CommonLedger\Sdk\HttpClient\Response
      */
-    public function view($addon_id, array $options = array())
+    public function view(array $options = array())
     {
         $query = (isset($options['query']) ? $options['query'] : array());
 
-        $response = $this->client->get($this->endpoint . '/' . $addon_id, $query, $options);
+        $response = $this->client->get($this->endpoint . '/' . $this->addon_id, $query, $options);
 
         return $response;
     }
